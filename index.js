0a1e0c0e431dfb833dbb65cc2767b8bf32f957ed
Mahmudul Hasan Selim <mhselim103@gmail.com>
2022-12-19 20:34:02 +0600

    first commit

--- index.js
+++ index.js
@@ -25,27 +25,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("luxuryLiving");
-    const servicesCollection = database.collection("services");
-    const emailCollection = database.collection("clientEmails");
-    const ordersCollection = database.collection("orders");
+    const database = client.db("jobTask");
+    const optionsCollection = database.collection("optionsCollection");
     const usersCollection = database.collection("users");
-    const reviewsCollection = database.collection("reviews");
 
     // Get all Services api
-    app.get("/services", async (req, res) => {
-      const cursor = servicesCollection.find({});
-      const services = await cursor.limit(6).toArray();
+    app.get("/options", async (req, res) => {
+      const cursor = optionsCollection.find({});
+      const options = await cursor.toArray();
       // console.log(services);
-      res.json(services);
-    });
-    // Get single service
-    app.get("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: objectId(id) };
-      const package = await servicesCollection.findOne(query);
-      // console.log("hello from inside dynamic route");
-      res.json(package);
+      res.json(options);
     });
 
     // get users
@@ -55,34 +44,18 @@ async function run() {
       // console.log(users);
       res.json(users);
     });
-    // email section
-    app.get("/clientEmails", async (req, res) => {
-      const cursor = emailCollection.find({});
-      const emails = await cursor.toArray();
-      // console.log(emails);
-      res.json(emails);
+    app.get("/users/:name", async (req, res) => {
+      const id = req.params.name;
+      const query = { name: id };
+      const package = await usersCollection.findOne(query);
+      console.log("hello from inside dynamic route");
+      res.json(package);
     });
-
-    // Get all ordersCollection
-    app.get("/orders", async (req, res) => {
-      const cursor = ordersCollection.find({});
-      const orders = await cursor.toArray();
-      // console.log(orders);
-      res.json(orders);
-    });
-
-    // Get Specific Orders
-    app.get("/myorders/:email", async (req, res) => {
-      const cursor = ordersCollection.find({ email: req.params.email });
-      const orders = await cursor.toArray();
-      res.json(orders);
-    });
-
     // users management
 
     app.post("/users", async (req, res) => {
       const user = req.body;
-      // console.log(user);
+      console.log(user);
       const result = await usersCollection.insertOne(user);
       // console.log("user inserted", user);
       res.json(result);
@@ -90,7 +63,7 @@ async function run() {
 
     app.put("/users", async (req, res) => {
       const user = req.body;
-      const filter = { email: user.email };
+      const filter = { name: user.name };
       const options = { upsert: true };
       const updateDoc = { $set: user };
       const result = await usersCollection.updateOne(
@@ -100,77 +73,6 @@ async function run() {
       );
       res.json(result);
     });
-
-    // making admin
-    app.put("/users/admin", async (req, res) => {
-      const user = req.body;
-      console.log("Put", user);
-      const filter = { email: user.email };
-      const updateDoc = { $set: { role: "admin" } };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.json(result);
-    });
-    // finding admin
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      // console.log(email);
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      let isAdmin = false;
-      if (user?.role === "admin") {
-        isAdmin = true;
-      }
-      res.json({ admin: isAdmin });
-    });
-
-    // order booking
-    app.post("/orders", async (req, res) => {
-      const order = req.body;
-      const result = await ordersCollection.insertOne(order);
-      // console.log(result);
-      res.json(result);
-    });
-    // add new Service
-    app.post("/services", async (req, res) => {
-      console.log("hitting service post");
-      const result = await servicesCollection.insertOne(req.body);
-      res.json(result);
-    });
-
-    //add subscription email
-    app.post("/clientEmails", async (req, res) => {
-      const result = await emailCollection.insertOne(req.body);
-      // console.log(result);
-      res.json(result);
-    });
-
-    // delete operation
-    app.delete("/orders/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: objectId(id) };
-      const result = await ordersCollection.deleteOne(query);
-      res.json(result);
-    });
-
-    app.delete("/myorders/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: objectId(id) };
-      const result = await ordersCollection.deleteOne(query);
-      res.json(result);
-    });
-
-    // review post and get
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review);
-      console.log(review);
-      res.json(result);
-    });
-    app.get("/reviews", async (req, res) => {
-      const cursor = reviewsCollection.find({});
-      const reviews = await cursor.toArray();
-      res.send(reviews);
-    });
   } finally {
     // await client.close();
   }
@@ -178,7 +80,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Luxury Living");
+  res.send("job task");
 });
 
 app.listen(port, () => {
